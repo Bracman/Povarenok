f406278341061996fc177aaf1e0adc7116dee69e
Динар Ямилев <dinar0509000@gmail.com>
2023-06-17 12:55:03 +0500

    Добавлены новые функции

--- Povarenok/CashierPanel.xaml.cs
+++ Povarenok/CashierPanel.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace Povarenok
 {
@@ -21,7 +23,94 @@ namespace Povarenok
     {
         public CashierPanel()
         {
-            InitializeComponent();
+            InitializeComponent();           
+           
+        }
+
+        private IList<Product> products = new List<Product>();
+
+
+        public void  loadProducts()
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+
+                dataTable.Rows.Clear();
+                ConnectionDataBase dataBase = new ConnectionDataBase();
+                dataTable = dataBase.StoredProcedureNotParametr("selectProducts");
+               
+                foreach (DataRow dr in dataTable.Rows)
+                {
+                    products.Add(new Product
+                    {
+                        nameProduct = dr[0].ToString()
+                    });
+
+                }
+                listDates.ItemsSource = products;        
+
+                
+
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.GetBaseException().ToString(), "Error",
+                           MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public class Product
+        {
+            public string nameProduct { get; set; }
+            public bool IsSelected { get; set; }
+        }
+
+      
+        public void addColumnDataGrid(string [] headers)
+        {
+            dataGrid.Columns.Clear();
+            foreach (string header in headers)
+            {
+                DataGridTextColumn c = new DataGridTextColumn();
+                c.Header = header;
+                dataGrid.AutoGenerateColumns = false;
+                dataGrid.CanUserResizeColumns = false;
+                dataGrid.CanUserResizeRows = false;
+                dataGrid.Columns.Add(c);
+                
+               
+            }
+        }
+
+        private void DishRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            loadProducts();
+            
+        }
+
+        private void ProductRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void activateSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedPassages = products.Where(ps => ps.IsSelected == true).ToList();
+            string message="";
+            foreach (var passage in selectedPassages)
+            {
+                message += passage.nameProduct + ",";
+            }
+            MessageBox.Show(message);
+
+            DataTable dataTable = new DataTable();
+                        
+            dataTable.Rows.Clear();
+            ConnectionDataBase dataBase = new ConnectionDataBase();
+            dataTable = dataBase.StoredProcedure("find_dishes","input", message);
+            dataGrid.ItemsSource = dataTable.DefaultView;
+
+
         }
     }
 }
---
+++ Povarenok/ConnectionDataBase.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows;
+
+namespace Povarenok
+{
+
+
+    class ConnectionDataBase
+    {
+        MySqlConnection mySql = new MySqlConnection(@"Server=localhost;Port=3306;UserId=root;Password=123456;Database=db_amoll");
+
+        public void openConnection()
+        {
+            if (mySql.State == System.Data.ConnectionState.Closed)
+            {
+                mySql.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (mySql.State == System.Data.ConnectionState.Open)
+            {
+                mySql.Close();
+            }
+        }
+
+        public MySqlConnection GetNpgsqlConnection()
+        {
+            return mySql;
+        }
+
+        public DataTable StoredProcedureNotParametr(string nameProcedure)
+        {
+            MySqlCommand mySqlComm = new MySqlCommand();
+
+            MySqlDataAdapter myadapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+            try
+            {
+                mySqlComm = new MySqlCommand(nameProcedure, mySql);
+                mySqlComm.CommandType = CommandType.StoredProcedure;
+                myadapter.SelectCommand = mySqlComm;
+                myadapter.Fill(table);
+
+
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.GetBaseException().ToString(), "Error",
+                           MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                mySqlComm.Dispose();
+                mySql.Close();
+            }
+            return table;
+        }
+        public DataTable StoredProcedure(string nameProcedure, string nameParametr,string nameValue)
+        {
+            MySqlCommand mySqlComm = new MySqlCommand();
+
+            MySqlDataAdapter myadapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+            try
+            {
+                mySqlComm = new MySqlCommand(nameProcedure, mySql);
+                mySqlComm.CommandType = CommandType.StoredProcedure;
+                mySqlComm.Parameters.AddWithValue(nameParametr,nameValue);
+                mySqlComm.Connection.Open();
+                mySqlComm.ExecuteReader();
+                myadapter.SelectCommand = mySqlComm;
+                myadapter.Fill(table);
+
+
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.GetBaseException().ToString(), "Error",
+                           MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                mySqlComm.Dispose();
+                mySql.Close();
+            }
+            return table;
+        }
+    }
+}
+
